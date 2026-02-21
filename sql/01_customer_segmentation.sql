05c31f770c4c72adecd96c9cf70d9cba2dd0c36c
aashkatanvi <155514768+aashkatanvi@users.noreply.github.com>
2026-02-22 02:16:54 +0530

    Update 01_customer_segmentation.sql

--- sql/01_customer_segmentation.sql
+++ sql/01_customer_segmentation.sql
@@ -6,9 +6,9 @@ WITH customer_segmentation AS (
 SELECT
          c.customer_unique_id,
          CASE
-                 WHEN COUNT(o.order_id) = 1 THEN 'One-time'
-                 ELSE 'Repeat'
-                 END AS customer_type
+                  WHEN COUNT(o.order_id) = 1 THEN 'One-time'
+                  ELSE 'Repeat'
+                  END AS customer_type
 FROM olist_project.orders o
 JOIN olist_project.customers c
          ON o.customer_id = c.customer_id
@@ -17,31 +17,32 @@ GROUP BY c.customer_unique_id
 )
 , order_revenue AS (
 SELECT
-oi.order_id,
-SUM(oi.price) AS order_revenue
+         oi.order_id,
+         SUM(oi.price) AS order_revenue
 FROM olist_project.order_item oi
 JOIN olist_project.orders o
-ON oi.order_id = o.order_id
+         ON oi.order_id = o.order_id
 WHERE order_status = 'delivered'
 GROUP BY oi.order_id
 )
 , orders_with_type AS (
 SELECT
-o.order_id,
-cs.customer_type
+         o.order_id,
+         cs.customer_type
 FROM olist_project.orders o
 JOIN olist_project.customers c
-ON o.customer_id = c.customer_id
+         ON o.customer_id = c.customer_id
 JOIN customer_segmentation cs
-ON c.customer_unique_id = cs.customer_unique_id
+         ON c.customer_unique_id = cs.customer_unique_id
 WHERE o.order_status = 'delivered'
 )
 SELECT
-owt.customer_type,
-COUNT(DISTINCT owt.order_id) AS total_orders,
-SUM(orv.order_revenue) AS total_revenue,
-Round(AVG(orv.order_revenue),2) AS avg_order_value
+         owt.customer_type,
+         COUNT(DISTINCT owt.order_id) AS total_orders,
+         SUM(orv.order_revenue) AS total_revenue,
+         Round(AVG(orv.order_revenue),2) AS avg_order_value
 FROM orders_with_type owt
 JOIN order_revenue orv
-ON owt.order_id = orv.order_id
+         ON owt.order_id = orv.order_id
 GROUP BY owt.customer_type;
+
