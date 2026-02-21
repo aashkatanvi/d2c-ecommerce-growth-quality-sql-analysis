a5fab87d0d2f3e47229824ed4d53bba48e918926
aashkatanvi <155514768+aashkatanvi@users.noreply.github.com>
2026-02-22 02:23:29 +0530

    Update 06_freight_efficiency_analysis.sql

--- sql/06_freight_efficiency_analysis.sql
+++ sql/06_freight_efficiency_analysis.sql
@@ -5,32 +5,30 @@
 WITH order_level AS
 (
 SELECT
-o.order_id,
-SUM(ot.price) AS order_revenue,
-SUM(ot.freight_value) AS order_freight
+      o.order_id,
+      SUM(ot.price) AS order_revenue,
+      SUM(ot.freight_value) AS order_freight
 FROM olist_project.order_item AS ot
 JOIN olist_project.orders AS o
-ON o.order_id = ot.order_id
+      ON o.order_id = ot.order_id
 WHERE o.order_status = 'delivered'
 GROUP BY o.order_id
 )
-
 SELECT
-c.customer_city,
-COUNT(ol.order_id) AS total_orders,
-SUM(ol.order_revenue) AS total_revenue,
-SUM(ol.order_freight) AS total_freight,
-ROUND(
-SUM(ol.order_freight) / NULLIF(SUM(ol.order_revenue), 0) * 100,
-2
-) AS freight_pct_of_revenue,
-ROUND (SUM(ol.order_revenue)/  COUNT(ol.order_id),2) AS AOV
-
+      c.customer_city,
+      COUNT(ol.order_id) AS total_orders,
+      SUM(ol.order_revenue) AS total_revenue,
+      SUM(ol.order_freight) AS total_freight,
+      ROUND(
+            SUM(ol.order_freight) / NULLIF(SUM(ol.order_revenue), 0) * 100,
+            2
+            ) AS freight_pct_of_revenue,
+      ROUND (SUM(ol.order_revenue)/  COUNT(ol.order_id),2) AS AOV
 FROM order_level AS ol
 JOIN olist_project.orders AS o
-ON ol.order_id = o.order_id
+      ON ol.order_id = o.order_id
 JOIN olist_project.customers AS c
-ON c.customer_id = o.customer_id
+      ON c.customer_id = o.customer_id
 GROUP BY c.customer_city
-
 ORDER BY freight_pct_of_revenue DESC;
+
