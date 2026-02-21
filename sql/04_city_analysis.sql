2c1c76ca1471adedf84fa4b8cf1c71f9324d920c
aashkatanvi <155514768+aashkatanvi@users.noreply.github.com>
2026-02-22 02:21:16 +0530

    Update 04_city_analysis.sql

--- sql/04_city_analysis.sql
+++ sql/04_city_analysis.sql
@@ -4,30 +4,31 @@
 
 WITH order_level_revenue AS
 (SELECT
-o.order_id,
-SUM(ot.price) order_revenue
+      o.order_id,
+      SUM(ot.price) order_revenue
 FROM olist_project.order_item ot
 JOIN olist_project.orders o
-ON ot.order_id= o.order_id
+      ON ot.order_id= o.order_id
 WHERE o.order_status = 'delivered'
 GROUP BY o.order_id
 )
 , revenue_by_city AS
 (
 SELECT
-c.customer_city,
-olr.order_id,
-olr.order_revenue
+      c.customer_city,
+      olr.order_id,
+      olr.order_revenue
 FROM order_level_revenue olr
 JOIN olist_project.orders o
-ON olr.order_id = o.order_id
+      ON olr.order_id = o.order_id
 JOIN olist_project.customers c
-ON o.customer_id = c.customer_id
+      ON o.customer_id = c.customer_id
 )
 SELECT
-customer_city,
-SUM(order_revenue) total_revenue,
-COUNT(order_id) total_orders,
-ROUND(SUM(order_revenue)/COUNT(order_id),2) average_order_value
+      customer_city,
+      SUM(order_revenue) total_revenue,
+      COUNT(order_id) total_orders,
+      ROUND(SUM(order_revenue)/COUNT(order_id),2) average_order_value
 FROM revenue_by_city
 GROUP BY customer_city;
+
