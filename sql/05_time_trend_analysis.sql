3d5730e20eb7a9ee36dbf24efd41acf5ca0bdbc8
aashkatanvi <155514768+aashkatanvi@users.noreply.github.com>
2026-02-22 02:21:50 +0530

    Update 05_time_trend_analysis.sql

--- sql/05_time_trend_analysis.sql
+++ sql/05_time_trend_analysis.sql
@@ -4,21 +4,22 @@
 
 WITH order_level_revenue AS
 (SELECT
-o.order_id,
-SUM(ot.price) order_revenue
+      o.order_id,
+      SUM(ot.price) order_revenue
 FROM olist_project.order_item ot
 JOIN olist_project.orders o
-ON ot.order_id= o.order_id
+      ON ot.order_id= o.order_id
 WHERE o.order_status = 'delivered'
 GROUP BY o.order_id
 )
 SELECT
-DATE_TRUNC('month', o.order_purchased_at) order_month,
-SUM(olr.order_revenue) total_revenue,
-COUNT(o.order_id) total_orders,
-ROUND(SUM(olr.order_revenue)/COUNT(o.order_id), 2) average_order_value
+      DATE_TRUNC('month', o.order_purchased_at) order_month,
+      SUM(olr.order_revenue) total_revenue,
+      COUNT(o.order_id) total_orders,
+      ROUND(SUM(olr.order_revenue)/COUNT(o.order_id), 2) average_order_value
 FROM order_level_revenue olr
 JOIN olist_project.orders o
-ON olr.order_id = o.order_id
+      ON olr.order_id = o.order_id
 GROUP BY DATE_TRUNC('month', o.order_purchased_at)
 ORDER BY order_month;
+
